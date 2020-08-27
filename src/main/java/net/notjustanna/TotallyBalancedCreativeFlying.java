7db977afa99da55e931d7c5b5a5d833bd453a1eb
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2020-08-27 01:26:44 -0300

    Initial code.

---
+++ src/main/java/net/notjustanna/TotallyBalancedCreativeFlying.java
@@ -0,0 +1,8 @@
+package net.notjustanna;
+
+import io.github.ladysnake.pal.AbilitySource;
+import io.github.ladysnake.pal.Pal;
+
+public class TotallyBalancedCreativeFlying {
+    public static AbilitySource abilitySource = Pal.getAbilitySource("tbcf", "communism");
+}
