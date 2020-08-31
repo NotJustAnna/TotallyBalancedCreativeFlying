5fec9fe2c537abd2df973cb9bacdaf7cc27d7db9
AdrianTodt <adriantodt.ms@gmail.com>
2020-08-31 18:54:24 -0300

    Fixes #1

--- src/main/java/net/adriantodt/mixin/PlayerManagerMixin.java
+++ src/main/java/net/adriantodt/mixin/PlayerManagerMixin.java
@@ -9,11 +9,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
     @Inject(at = @At("TAIL"), method = "onPlayerConnect")
     private void playerSpawned(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
         TotallyBalancedCreativeFlying.abilitySource.grantTo(player, VanillaAbilities.ALLOW_FLYING);
+    }
+
+    @Inject(at = @At("RETURN"), method = "respawnPlayer")
+    private void respawnPlayer(ServerPlayerEntity player, boolean end, CallbackInfoReturnable<ServerPlayerEntity> cir) {
+        TotallyBalancedCreativeFlying.abilitySource.grantTo(cir.getReturnValue(), VanillaAbilities.ALLOW_FLYING);
     }
 }
